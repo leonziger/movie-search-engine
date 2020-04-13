7b2332dab9df6e15224b06a6b0767257ad6a3528
Uma <buldog52@gmail.com>
2020-04-13 15:42:47 +0300

    Updates

---
+++ precache-manifest.0c06dd9f41e26c18f723cf33d3edfdbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1550454a251e2101f526641e24a2fb8f",
+    "url": "/movie-search-engine/index.html"
+  },
+  {
+    "revision": "1b402669d0cf79609e2e",
+    "url": "/movie-search-engine/static/css/2.2831ab12.chunk.css"
+  },
+  {
+    "revision": "73a4f10cc5e49b8f7af2",
+    "url": "/movie-search-engine/static/css/main.51b028b3.chunk.css"
+  },
+  {
+    "revision": "1b402669d0cf79609e2e",
+    "url": "/movie-search-engine/static/js/2.47e676af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-engine/static/js/2.47e676af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a4f10cc5e49b8f7af2",
+    "url": "/movie-search-engine/static/js/main.3f1cb416.chunk.js"
+  },
+  {
+    "revision": "9db9275d2c4ea314b3eb",
+    "url": "/movie-search-engine/static/js/runtime-main.3cb13f0f.js"
+  }
+]);
